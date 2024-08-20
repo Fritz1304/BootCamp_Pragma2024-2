71d51dc62a50d2d980b88e155a6d0cfbb9d37db4
Fritz1304 <freetoplay1304@gmail.com>
2024-08-20 17:18:13 -0500

    Commit inicial con estructura básica del proyecto

---
+++ src/main/java/com/Bootcamp/MicroservicioStock/MicroservicioStockApplication.java
@@ -0,0 +1,13 @@
+package com.Bootcamp.MicroservicioStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Bootcamp/MicroservicioStock/MicroservicioStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bootcamp.MicroservicioStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
